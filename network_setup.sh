2ba3b2b12d21a555ded086f97cee52ec3b49d4df
Ratnakar <asara.ratnakar@gmail.com>
2017-09-29 11:49:47 -0400

    Update network_setup.sh

--- network_setup.sh
+++ network_setup.sh
@@ -60,11 +60,8 @@ function networkUp () {
       source generateArtifacts.sh $CH_NAME
     fi
 
-    if [ "${IF_COUCHDB}" == "couchdb" ]; then
-      CHANNEL_NAME=$CH_NAME TIMEOUT=$CLI_TIMEOUT docker-compose -f $COMPOSE_FILE -f $COMPOSE_FILE_COUCH up -d 2>&1
-    else
-      CHANNEL_NAME=$CH_NAME TIMEOUT=$CLI_TIMEOUT docker-compose -f $COMPOSE_FILE up -d 2>&1
-    fi
+    CHANNEL_NAME=$CH_NAME TIMEOUT=$CLI_TIMEOUT docker-compose -f $COMPOSE_FILE -f $COMPOSE_FILE_COUCH up -d 2>&1
+
     if [ $? -ne 0 ]; then
 	echo "ERROR !!!! Unable to pull the images "
 	exit 1
